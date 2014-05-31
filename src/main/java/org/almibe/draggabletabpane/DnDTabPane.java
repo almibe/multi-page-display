5a95921e47dcf5c1e3d737e8616f0f5c6c1e4e52
Alex Berry <berry3@indiana.edu>
2014-05-31 13:32:15 -0400

    Removing addTab method and switching to dev version of TabPaneSkin

--- src/main/java/org/almibe/draggabletabpane/DnDTabPane.java
+++ src/main/java/org/almibe/draggabletabpane/DnDTabPane.java
@@ -1,7 +1,6 @@
 package org.almibe.draggabletabpane;
 
 import javafx.scene.control.Skin;
-import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 
 public class DnDTabPane extends TabPane {
@@ -11,12 +10,6 @@ public class DnDTabPane extends TabPane {
 
     @Override
     protected Skin<?> createDefaultSkin() {
-        return new TabPaneSkin(this);
-//        return new TabPaneSkinNew(this);
-    }
-
-    public void addTab(Tab t) {
-        this.getTabs().add(t);
+        return new TabPaneSkinNew(this);
     }
 }
-
