26d6f9eb1ac517d6723024cdcb0b64adea4b9157
Alex Berry <berry3@indiana.edu>
2016-03-30 18:58:08 -0400

    adding selected tab property

--- src/main/java/org/almibe/controls/DraggableTabPane.java
+++ src/main/java/org/almibe/controls/DraggableTabPane.java
@@ -1,5 +1,7 @@
 package org.almibe.controls;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.Control;
@@ -8,6 +10,8 @@ import org.almibe.controls.skins.DraggableTabPaneSkin;
 
 public class DraggableTabPane extends Control {
     private final ObservableList<DraggableTab> tabs = FXCollections.observableArrayList();
+    //TODO maybe replace below with ObjectProperty<SingleSelectionModel<DraggableTab>> like TabPane uses?
+    private final ObjectProperty<DraggableTab> selectedTab = new SimpleObjectProperty<>();
 
     @Override
     protected Skin<?> createDefaultSkin() {
@@ -16,5 +20,17 @@ public class DraggableTabPane extends Control {
 
     public ObservableList<DraggableTab> getTabs() {
         return tabs;
+    }
+
+    public DraggableTab getSelectedTab() {
+        return selectedTab.get();
+    }
+
+    public ObjectProperty<DraggableTab> selectedTabProperty() {
+        return selectedTab;
+    }
+
+    public void setSelectedTab(DraggableTab selectedTab) {
+        this.selectedTab.set(selectedTab);
     }
 }
