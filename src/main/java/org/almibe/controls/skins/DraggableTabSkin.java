915f9eb2b6f0f1929b945b81916103cca54bca2a
Alex Berry <berry3@indiana.edu>
2016-03-30 00:57:52 -0400

    DraggableTab has a very rough tab shape, good enough for now.

--- src/main/java/org/almibe/controls/skins/DraggableTabSkin.java
+++ src/main/java/org/almibe/controls/skins/DraggableTabSkin.java
@@ -1,9 +1,11 @@
 package org.almibe.controls.skins;
 
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.SkinBase;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import org.almibe.controls.DraggableTab;
 
 public class DraggableTabSkin extends SkinBase<DraggableTab> {
@@ -15,6 +17,16 @@ public class DraggableTabSkin extends SkinBase<DraggableTab> {
         super(draggableTab);
         text.textProperty().bind(draggableTab.textProperty());
         layout.getChildren().addAll(text, closeButton);
-        getChildren().add(layout);
+        layout.setPadding(new Insets(10d));
+        layout.setSpacing(10d);
+        layout.setBorder(createBorder());
+        FlowPane region = new FlowPane();
+        region.getChildren().addAll(layout);
+        getChildren().add(region);
+    }
+
+    private Border createBorder() {
+        return new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID,
+            new CornerRadii(10d, 10d, 0d, 0d, false), BorderWidths.DEFAULT));
     }
 }
