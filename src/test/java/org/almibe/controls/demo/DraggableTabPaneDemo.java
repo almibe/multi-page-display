cbf7b6b8cabf742d6d362a77aa7e5300fe046f07
Alex Berry <berry3@indiana.edu>
2016-03-30 01:03:22 -0400

    filling in demo to test header of DraggableTabPane

--- src/test/java/org/almibe/controls/demo/DraggableTabPaneDemo.java
+++ src/test/java/org/almibe/controls/demo/DraggableTabPaneDemo.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.almibe.controls.DraggableTab;
+import org.almibe.controls.DraggableTabPane;
 
 public class DraggableTabPaneDemo extends Application {
     public static void main(String[] args) {
@@ -12,9 +13,15 @@ public class DraggableTabPaneDemo extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        DraggableTabPane draggableTabPane = new DraggableTabPane();
         DraggableTab draggableTab = new DraggableTab("Hello Demo");
+        DraggableTab draggableTab2 = new DraggableTab("Hello Demo?");
+        DraggableTab draggableTab3 = new DraggableTab("Hello Demo!");
+        DraggableTab draggableTab4 = new DraggableTab("Hello Demo?!?!!");
 
-        primaryStage.setScene(new Scene(draggableTab, 300, 250));
+        draggableTabPane.getTabs().addAll(draggableTab, draggableTab2, draggableTab3, draggableTab4);
+
+        primaryStage.setScene(new Scene(draggableTabPane, 300, 250));
         primaryStage.show();
     }
 }
