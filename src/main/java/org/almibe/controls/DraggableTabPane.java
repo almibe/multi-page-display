9906b72e64ef7a72bd39d4ac22b1ec78cab16b01
Alex Berry <berry3@indiana.edu>
2016-03-30 01:02:44 -0400

    adding Tabs property for DraggableTabPane

--- src/main/java/org/almibe/controls/DraggableTabPane.java
+++ src/main/java/org/almibe/controls/DraggableTabPane.java
@@ -1,12 +1,20 @@
 package org.almibe.controls;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.control.Control;
 import javafx.scene.control.Skin;
 import org.almibe.controls.skins.DraggableTabPaneSkin;
 
 public class DraggableTabPane extends Control {
+    private final ObservableList<DraggableTab> tabs = FXCollections.observableArrayList();
+
     @Override
     protected Skin<?> createDefaultSkin() {
         return new DraggableTabPaneSkin(this);
+    }
+
+    public ObservableList<DraggableTab> getTabs() {
+        return tabs;
     }
 }
