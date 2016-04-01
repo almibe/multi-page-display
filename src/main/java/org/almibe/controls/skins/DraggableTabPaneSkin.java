0837451a211077484a18fe674103d7b7fab04ef4
Alex Berry <berry3@indiana.edu>
2016-03-31 20:01:09 -0400

    renaming and adding place for arrow controls

--- src/main/java/org/almibe/controls/skins/DraggableTabPaneSkin.java
+++ src/main/java/org/almibe/controls/skins/DraggableTabPaneSkin.java
@@ -13,26 +13,42 @@ import java.util.function.Function;
 
 public class DraggableTabPaneSkin extends SkinBase<DraggableTabPane> {
 
-    private final ScrollPane header = new ScrollPane();
-    private final HBox headerContent = new HBox();
+    private final ScrollPane tabScrollPane = new ScrollPane();
+    private final HBox tabArea = new HBox();
+    private final HBox arrowsControls = new HBox();
+    private final BorderPane header = new BorderPane();
     private final ScrollPane content = new ScrollPane();
     private final BorderPane tabPane = new BorderPane();
 
     public DraggableTabPaneSkin(DraggableTabPane draggableTabPane) {
         super(draggableTabPane);
 
-        Bindings.bindContent(headerContent.getChildren(), MappedList.map(draggableTabPane.getTabs(),
+        Bindings.bindContent(tabArea.getChildren(), MappedList.map(draggableTabPane.getTabs(),
             (Function <DraggableTab, Node>) draggableTab -> new DraggableTabSkin(draggableTab, this)));
 
         content.contentProperty().bind(Bindings.select(draggableTabPane.selectedTabProperty(), "content"));
 
-        header.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        header.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        header.contentProperty().setValue(headerContent);
+        tabScrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        tabScrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        tabScrollPane.contentProperty().setValue(tabArea);
+
+        header.setCenter(tabScrollPane);
+        header.setRight(arrowsControls);
 
         tabPane.setTop(header);
         tabPane.setCenter(content);
 
+        tabArea.widthProperty().addListener((observable, oldValue, newValue) -> checkArrows());
+        tabScrollPane.widthProperty().addListener((observable, oldValue, newValue) -> checkArrows());
+
         this.getChildren().add(tabPane);
+    }
+
+    private void checkArrows() {
+        if (tabArea.getWidth() > tabScrollPane.getWidth()) {
+            System.out.println("show arrowsControls");
+        } else {
+            System.out.println("hide arrowsControls");
+        }
     }
 }
