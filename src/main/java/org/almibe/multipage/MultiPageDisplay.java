2311d2c9ec8e187fd34c3470e83237dccf756210
Alex Berry <berry3@indiana.edu>
2016-04-03 00:54:27 -0400

    removing observable pages from MultiPageDisplay api

--- src/main/java/org/almibe/multipage/MultiPageDisplay.java
+++ src/main/java/org/almibe/multipage/MultiPageDisplay.java
@@ -1,6 +1,7 @@
 package org.almibe.multipage;
 
 import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.ReadOnlyListProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -9,21 +10,30 @@ import javafx.scene.control.Skin;
 import org.almibe.multipage.skins.MultiPageDisplaySkin;
 
 public class MultiPageDisplay extends Control {
-    private final ObservableList<Page> pages = FXCollections.observableArrayList();
     private final ObjectProperty<Page> selectedPage = new SimpleObjectProperty<>();
     private final DefaultPageFactory defaultPageFactory;
+    private final MultiPageDisplaySkin multiPageDisplaySkin;
 
     public MultiPageDisplay(DefaultPageFactory defaultPageFactory) {
+        multiPageDisplaySkin = new MultiPageDisplaySkin(this);
         this.defaultPageFactory = defaultPageFactory;
     }
 
     @Override
     protected Skin<?> createDefaultSkin() {
-        return new MultiPageDisplaySkin(this);
+        return multiPageDisplaySkin;
     }
 
-    public ObservableList<Page> getPages() {
-        return pages;
+    public ReadOnlyListProperty<Page> getPages() {
+        return multiPageDisplaySkin.getPages();
+    }
+
+    public void addPage(Page page) {
+        multiPageDisplaySkin.addPage(page);
+    }
+
+    public void removePage(Page page) {
+        multiPageDisplaySkin.removePage(page);
     }
 
     public Page getSelectedPage() {
