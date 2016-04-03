36e2d84bf454a380151e4ea87e53a0662f4cf62f
Alex Berry <berry3@indiana.edu>
2016-04-03 15:21:53 -0400

    starting to add icon support

--- src/main/java/org/almibe/multipage/Page.java
+++ src/main/java/org/almibe/multipage/Page.java
@@ -7,22 +7,30 @@ import javafx.beans.property.StringProperty;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.Node;
+import javafx.scene.image.ImageView;
 
 public class Page {
     private final ObjectProperty<Node> content = new SimpleObjectProperty<>();
     private final StringProperty text = new SimpleStringProperty();
     private final EventHandler<Event> onCloseRequest;
+    private final ImageView imageView;
 
-    public Page(String title, Node node) {
+    public Page(String title, ImageView image, Node node) {
         text.setValue(title);
         content.setValue(node);
+        imageView = image;
         onCloseRequest = null;
     }
 
-    public Page(String title, Node node, EventHandler<Event> onCloseRequest) {
+    public Page(String title, ImageView image, Node node, EventHandler<Event> onCloseRequest) {
         text.setValue(title);
         content.setValue(node);
+        imageView = image;
         this.onCloseRequest = onCloseRequest;
+    }
+
+    public ImageView getImageView() {
+        return imageView;
     }
 
     public Node getContent() {
--- src/main/java/org/almibe/multipage/skins/PageTabNode.java
+++ src/main/java/org/almibe/multipage/skins/PageTabNode.java
@@ -4,14 +4,17 @@ import javafx.beans.property.ObjectProperty;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import org.almibe.multipage.Page;
 
-public class PageTabNode extends HBox {
+public class PageTabNode extends GridPane {
     private final Page page;
     private final Label text = new Label();
-    private final Button closeButton = new Button("X");
+    private final Image closeIcon = new Image(getClass().getResourceAsStream("famfamfamsilk/cross.png"));
+    private final Button closeButton = new Button();
     private final ObjectProperty<Page> selectedPage;
 
     public PageTabNode(Page page, ObjectProperty<Page> selectedPage, TabAreaNode tabAreaNode) {
@@ -19,9 +22,20 @@ public class PageTabNode extends HBox {
         this.page = page;
         this.selectedPage = selectedPage;
         text.textProperty().bind(page.textProperty());
-        getChildren().addAll(text, closeButton);
+        closeButton.setGraphic(new ImageView(closeIcon));
+
+        getColumnConstraints().add(new ColumnConstraints(32));
+        GridPane.setConstraints(page.getImageView(), 0, 0);
+
+        getColumnConstraints().add(new ColumnConstraints(500));
+        GridPane.setConstraints(text, 1, 0);
+
+        getColumnConstraints().add(new ColumnConstraints(32));
+        GridPane.setConstraints(closeButton, 2, 0);
+
+        getChildren().addAll(page.getImageView(), text, closeButton);
+
         setPadding(new Insets(10d));
-        setSpacing(10d);
         setBorder(createBorder());
         this.setOnMouseClicked(event -> {
             this.selectedPage.set(page);
