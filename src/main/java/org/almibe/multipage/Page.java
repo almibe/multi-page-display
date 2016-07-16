474c21918342b626dc3a9b28a2126833396057c7
Alex Berry <berry3@indiana.edu>
2016-07-16 11:31:25 -0400

    making title a ReadOnlyStringProperty and removing setters

--- src/main/java/org/almibe/multipage/Page.java
+++ src/main/java/org/almibe/multipage/Page.java
@@ -5,26 +5,26 @@
 package org.almibe.multipage;
 
 import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.ReadOnlyStringProperty;
 import javafx.beans.property.SimpleObjectProperty;
-import javafx.beans.property.StringProperty;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.Node;
 
 public class Page {
     private final ObjectProperty<Node> content = new SimpleObjectProperty<>();
-    private final StringProperty title;
+    private final ReadOnlyStringProperty title;
     private final EventHandler<Event> onCloseRequest;
     private final Node icon;
 
-    public Page(StringProperty title, Node icon, Node node) {
+    public Page(ReadOnlyStringProperty title, Node icon, Node node) {
         this.title = title;
         this.content.setValue(node);
         this.icon = icon;
         this.onCloseRequest = null;
     }
 
-    public Page(StringProperty title, Node icon, Node node, EventHandler<Event> onCloseRequest) {
+    public Page(ReadOnlyStringProperty title, Node icon, Node node, EventHandler<Event> onCloseRequest) {
         this.title = title;
         this.content.setValue(node);
         this.icon = icon;
@@ -43,20 +43,12 @@ public class Page {
         return content;
     }
 
-    public void setContent(Node node) {
-        content.setValue(node);
-    }
-
     public String getTitle() {
         return title.get();
     }
 
-    public StringProperty titleProperty() {
+    public ReadOnlyStringProperty titleProperty() {
         return title;
-    }
-
-    public void setTitle(String newValue) {
-        title.set(newValue);
     }
 
     public EventHandler<Event> getOnCloseRequest() {
